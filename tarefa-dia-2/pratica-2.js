148fa06b7c2d8b28a5e9127a41b0621f1b2e8d7c
Juliano <jumenani@gmail.com>
2022-08-29 10:34:37 -0300

    Commits tarefa 2

---
+++ tarefa-dia-2/pratica-2.js
@@ -0,0 +1,5 @@
+let nome = 'Juliano'
+let email = 'jmenani@gmail.com'
+let profissao = 'DBA'
+let idade = 40
+let lugarSonho = 'Viena'
---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,16 @@
+let nome = 'Juliano'
+let email = 'jmenani@gmail.com'
+let profissao = 'DBA'
+let idade = 40
+let lugarSonho = 'Viena'
+
+// Olá, <valor da variável que tem o seu nome>! Somos aqui da agência de turismo 
+// Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para 
+// <valor da variável que armazena o lugar que você sonha conhecer>. Confirma pra gente alguns dados? A sua idade é <valor 
+// da variável que armazena sua idade>, você é <valor da variável que armazena sua profissão> e o seu email é <valor da variável 
+// que armazena seu email>? Ficamos no aguardo. Muito obrigado e boa viagem!
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarSonho}. 
+Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? 
+Ficamos no aguardo. Muito obrigado e boa viagem!`)
